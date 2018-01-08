392b23fc43a2feeac83e0af131ea775d40e3f088
Piotr Nowacki <p.nowacki90@gmail.com>
2018-01-08 21:18:58 +0000

    Added create page, show page, index page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,12 +5,23 @@ class ArticlesController < ApplicationController
   end
   
   def show
-    
+    @article = Article.find(params[:id])
   end
   
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    
+    @article.save
+    flash[:notice] = "Post saved."
+    redirect_to @article
+  end
+  
+  def index
+    @articles = Article.all
   end
   
   private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
